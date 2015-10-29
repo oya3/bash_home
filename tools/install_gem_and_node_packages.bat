4b2e0bd7e17f95797a5251e6eb0f2017e4f33ef8
Kazunori OYA <terje@oya3.net>
2015-10-29 10:39:57 +0900

    bat file additional to intall the package.
    for gem and nodejs.

---
+++ tools/install_gem_and_node_packages.bat
@@ -0,0 +1,11 @@
+rem sass構文チェック(scss-mode&sass-mode)
+call gem install sass
+rem coffee構文チェック(coffee-mode)
+call npm install -g coffeelint
+rem js構文チェック(javascript-mode)
+call npm install -g jshint
+rem markdown リアルタイム表示(realtime-preview)
+call gem install redcarpet
+rem js整形(web-beautify)
+call npm -g install js-beautify
+
