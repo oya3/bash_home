02fae2a0c10faf1ce5ebb402009cc83e75c3b344
Kazunori OYA <terje@oya3.net>
2016-03-23 17:44:10 +0900

    go-mode追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -872,6 +872,23 @@ mouse-3: delete other windows"
           )
 
 ;;------------------------------------------------------------------------------
+;; Go+ mode
+(with-eval-after-load 'go-mode
+  ;; auto-complete
+  (require 'go-autocomplete)
+
+  ;; ;; company-mode
+  ;; (add-to-list 'company-backends 'company-go)
+
+  ;; eldoc
+  (add-hook 'go-mode-hook 'go-eldoc-setup)
+  )
+
+  ;; ;; key bindings
+  ;; (define-key go-mode-map (kbd "M-.") 'godef-jump)
+  ;; (define-key go-mode-map (kbd "M-,") 'pop-tag-mark))
+
+;;------------------------------------------------------------------------------
 ;; C/C++ mode
 ;; ;; Qt 考慮する
 ;; (setq c-C++-access-key "\\<\\(slots\\|signals\\|private\\|protected\\|public\\)\\>[ \t]*[(slots\\|signals)]*[ \t]*:")
